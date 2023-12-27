bfc251d452d922498d39abe85349be52f9ff015a
phey81pm <70926712+phey81pm@users.noreply.github.com>
2023-12-27 23:10:12 +0800

    up

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,25 @@ import Register from './components/Auth/Register';
 import Navbar from './Navbar';
 import Footer from './Footer';
 
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
 import { GoogleOAuthProvider  } from '@react-oauth/google'; 
 const clientID = "21072412527-addcofe4fdtgmqf7cvcc39tvu3hejc2r.apps.googleusercontent.com"; 
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyDJOQQf_lOPUfokwxnYR3iOyxpMbiYcGMc",
+  authDomain: "titanium-deck-286202.firebaseapp.com",
+  projectId: "titanium-deck-286202",
+  storageBucket: "titanium-deck-286202.appspot.com",
+  messagingSenderId: "21072412527",
+  appId: "1:21072412527:web:d11f9d8a1db6b51c07329a",
+  measurementId: "G-3XCJD6X57N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 function App() {   
   return (   
