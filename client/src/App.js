d68579403c1473a1fd68e9618691f9a3fa12d7e9
Ko͞owd <phey.81property@gmail.com>
2023-12-29 15:52:22 +0800

    up

--- client/src/App.js
+++ client/src/App.js
@@ -15,40 +15,37 @@ import Register from './components/Auth/Register';
 import Navbar from './Navbar';
 import Footer from './Footer'; 
 
-import { Auth } from './components/auth';
-
-import { GoogleOAuthProvider  } from '@react-oauth/google'; 
-const clientID = "21072412527-addcofe4fdtgmqf7cvcc39tvu3hejc2r.apps.googleusercontent.com"; 
-  
+import { AuthContextProvider } from './context/AuthContext';
+ 
 
 function App() {   
   return (    
-   
-    <GoogleOAuthProvider clientId={clientID}> 
-      <BrowserRouter>   
-        <div className='row'>
-            <div>
-              <Navbar />
-            </div>
-            <div className="container-fluid py-5 px-4">
-              <Routes>   
-                <Route path="/login" element={<Login />} />
-                <Route path="/logout" element={<Logout />} />
-                <Route path="/register" element={<Register />} /> 
- 
-                <Route path="/users/" element={<View />} />
+    
+        <BrowserRouter>   
+            <AuthContextProvider>
+              <div className='row'>
+                <div>
+                  <Navbar />
+                </div>
+                <div className="container-fluid py-5 px-4">
+                  <Routes>   
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/logout" element={<Logout />} />
+                    <Route path="/register" element={<Register />} /> 
 
-                <Route path="/users/create" element={<Create />} />
-                <Route path="/users/edit/:id" element={<Edit />} />
-                <Route path="/users/read/:id" element={<Read />} />  
-              </Routes>
-            </div>
-            <div className='mt-auto'>
-              <Footer />
-            </div>
-          </div>   
-      </BrowserRouter>  
-    </GoogleOAuthProvider>
+                    <Route path="/users/" element={<View />} />
+
+                    <Route path="/users/create" element={<Create />} />
+                    <Route path="/users/edit/:id" element={<Edit />} />
+                    <Route path="/users/read/:id" element={<Read />} />  
+                  </Routes>
+                </div>
+                <div className='mt-auto'>
+                  <Footer />
+                </div>
+              </div>   
+            </AuthContextProvider>
+        </BrowserRouter>   
   )
 }
 
--- client/src/components/Auth/Login.jsx
+++ client/src/components/Auth/Login.jsx
@@ -4,31 +4,44 @@ import {Link} from 'react-router-dom';
 import { auth , googleProvider} from "../../config/firebase";
 import { createUserWithEmailAndPassword,signInWithPopup, signOut } from "firebase/auth"; 
 
+import { UserAuth } from '../../context/AuthContext';
+
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    console.log(auth?.currentUser?.email);
-    const signIn = async () => {
+    // const [email, setEmail] = useState("");
+    // const [password, setPassword] = useState("");
+    // console.log(auth?.currentUser?.email);
+    // const signIn = async () => {
+    //     try {
+    //         await createUserWithEmailAndPassword(auth, email, password);
+    //     } catch (err){
+    //         console.error(err);
+    //     }
+    // };
+    // const signInWithGoogle = async () => {
+    //     try {
+    //         await signInWithPopup(auth,googleProvider);
+    //     } catch (err){
+    //         console.error(err);
+    //     }
+    // };
+    // const logOut = async () => {
+    //     try {
+    //         await signOut(auth);
+    //     } catch (err){
+    //         console.error(err);
+    //     }
+    // };
+
+    
+
+    const handleGoogleSignIn = async () => {
         try {
-            await createUserWithEmailAndPassword(auth, email, password);
-        } catch (err){
-            console.error(err);
+            await googleSignIn();
+        } catch (error) {
+            console.log(error)
         }
-    };
-    const signInWithGoogle = async () => {
-        try {
-            await signInWithPopup(auth,googleProvider);
-        } catch (err){
-            console.error(err);
-        }
-    };
-    const logOut = async () => {
-        try {
-            await signOut(auth);
-        } catch (err){
-            console.error(err);
-        }
-    };
+
+    }
 
 
   return ( 
